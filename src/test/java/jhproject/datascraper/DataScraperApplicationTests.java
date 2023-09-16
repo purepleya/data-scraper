b5704081185ef798b3da0788776b999202e7fcec
jh <purepleya@gmail.com>
2023-09-16 18:56:25 +0900

    init project

---
+++ src/test/java/jhproject/datascraper/DataScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package jhproject.datascraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
